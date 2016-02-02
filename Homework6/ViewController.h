e14219ba87e96b7d3ebf9a5e5bd2f58f11ac26b5
Admin <admin@mac-admin.local>
2016-02-02 21:53:36 +0200

    Initial Commit

---
+++ Homework6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework6
+//
+//  Created by Admin on 02.02.16.
+//  Copyright © 2016 Ehlakov Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
